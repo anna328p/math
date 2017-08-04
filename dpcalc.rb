c6e8cf7f98aba037d31259b6e7e1b54fdc0b86c9
Dmitry Kudriavtsev <me@dk0.us>
2017-08-03 23:26:39 -0700

    Added dot product calculator

---
+++ dpcalc.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# Dot Product Calculator
+# by Dmitry Kudriavtsev. license: GPL
+
+usage = "Usage: [program] <arrays' length> <array 1> <array 2>"
+
+abort usage if ARGV.length < 2
+n = ARGV.shift.to_i
+abort usage if ARGV.length != n*2
+
+array1 = ARGV.slice 0, n
+array2 = ARGV.slice n, ARGV.length
+
+result = array1.map(:to_i).zip(array2.map(:to_i)).map(:*).reduce(:+)
+
+puts "Result: #{result}"
