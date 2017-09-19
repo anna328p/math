aa548d0bfc8d2bf74f8051036fad22fc8aac07a7
dkudriavtsev <dkudriavtsev@gmail.com>
2017-09-19 12:43:06 -0700

    Fixed bugs and added thing

--- Cramer2x2.rb
+++ Cramer2x2.rb
@@ -1,20 +1,22 @@
 require 'gtk3'
 
-builder_file = 'DeterminantLayout.glade'
+builder_file = 'Cramer2x2Layout.glade'
 
 builder = Gtk::Builder.new file: builder_file
 
-window = builder.get_object "Window1"
+window = builder.get_object "CramerWindow"
 window.signal_connect("destroy") { Gtk.main_quit }
 
-label = builder.get_object "DisplayOutput"
+label = builder.get_object "XOutput"
 
 input_a = builder.get_object "InputA"
 input_b = builder.get_object "InputB"
 input_c = builder.get_object "InputC"
 input_d = builder.get_object "InputD"
+input_e = builder.get_object "InputE"
+input_f = builder.get_object "InputF"
 
-button = builder.get_object "BtnCalculate"
+button = builder.get_object "CalculateButton"
 button.signal_connect("clicked") { 
 	a = input_a.text.to_r
 	b = input_b.text.to_r
