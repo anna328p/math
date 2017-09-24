4808e49a9527338ac65342f9ace675e6eefd96f1
Recovery User <recovery@tera.localdomain>
2017-09-23 23:52:31 -0700

    Changes

--- dot-product.rb
+++ dot-product.rb
@@ -12,6 +12,8 @@ abort usage if ARGV.length != n*2
 array1 = ARGV.slice 0, n
 array2 = ARGV.slice n, ARGV.length
 
-result = array1.map(&:to_f).zip(array2.map(&:to_f)).map{|a, b| a * b}.reduce(&:+)
+result = array1.map(&:to_r).zip(array2.map(&:to_r)).map{|a, b| a * b}.reduce(&:+)
+
+result = (result.to_f % 1 == 0) ? result.to_i : result
 
 puts "Result: #{result}"
